ae2bf937d03027ea48af6ead9c07f06d17648238
Mairbek Khadikov <mkhadikov@gmail.com>
2020-01-12 13:44:42 -0800

    temp

--- hor_vert.cc
+++ hor_vert.cc
@@ -12,6 +12,209 @@ struct Event {
   int type;
   int y1, y2;
 };
+int rand_num(int n) {
+  return std::rand() % n;
+}
+
+int rand_geometric(int max_num) {
+  int lev = 0;
+  while (lev < max_num && rand_num(2) == 0) {
+    lev++;
+  }
+  return lev;
+}
+
+class Node {
+ public:
+  Node(int value, int levels) : value_(value), levels_(levels) {
+    forward_ = new Node*[levels_+1];
+    for (int i = 0; i <= levels_; i++) {
+      forward_[i] = nullptr;
+    }
+    counts_ = new int[levels_+1];
+    for (int i = 0; i <= levels_; i++) {
+      counts_[i] = 0;
+    }
+  }
+
+  void print(int i) {
+    std::cout << " value [" << value_ << ", [" << counts_[i] << "]]";
+  }
+
+  int value() {
+    return value_;
+  }
+
+  void inc_count(int level) {
+    counts_[level]++;
+  }
+
+  void dec_count(int level) {
+    counts_[level]--;
+  }
+
+
+  int levels() { return levels_; }
+
+  Node* forward(int level) {
+    return forward_[level];
+  }
+
+  Node* next(int layer) {
+    return forward_[layer];
+  }
+
+  void set_forward(int level, Node* ptr, int cnt) {
+    forward_[level] = ptr;
+    counts_[level] = cnt;
+  }
+
+  int get_count(int level) {
+    return counts_[level];
+  }
+
+ private:
+  int value_;
+  int levels_;
+  int* counts_;
+  Node** forward_;
+};
+
+class SkipList {
+ public:
+  SkipList() : level_(1), size_(0) {
+    head_ = new Node(-1234, level_);
+  }
+
+  int size() { return size_; }
+
+  bool remove(int value) {
+    Node** update = new Node*[level_ + 1];
+    Node* ptr = head_;
+    Node* nxt = nullptr;
+    for (int i = level_; i >= 0; i--) {
+      while(true) {
+        nxt = ptr->forward(i);
+        if (nxt == nullptr) {
+          break;
+        }
+        if (value <= nxt->value()) {
+          break;
+        }
+        ptr = nxt;
+      }
+      update[i] = ptr;
+    }
+    if (nxt == nullptr || nxt->value() != value) {
+      return false;
+    }
+    bool rm = update[0]->get_count(0) == 1;
+    for (int i = 0; i <= level_; i++) {
+      int count = update[i]->get_count(i);
+      if (rm && update[i]->forward(i) == nxt) {
+        update[i]->set_forward(i, nxt->forward(i), count);
+      } else {
+        update[i]->dec_count(i);
+      }
+    }
+    if (rm) {
+      delete nxt;
+    }
+    size_--;
+    return true;
+  }
+
+  // http://code.activestate.com/recipes/576930/
+  void insert(int value) {
+    int new_level = rand_geometric(size_);
+    // std::cout << "adjust level from " << level_ << " to " << new_level << std::endl;
+    if (new_level > level_) {
+      auto new_head = new Node(-1234, new_level);
+      for (int i = 0; i <= level_; i++) {
+        new_head->set_forward(i, head_->forward(i), head_->get_count(i));
+      }
+      std::swap(head_, new_head);
+      delete new_head;
+      level_ = new_level;
+    }
+    Node** update = new Node*[level_ + 1];
+    Node* ptr = head_;
+    Node* nxt = nullptr;
+    int* widths = new int[level_ + 1];
+    for (int i = level_; i >= 0; i--) {
+      widths[i] = 0;
+      while(true) {
+        nxt = ptr->forward(i);
+        if (nxt == nullptr) {
+          break;
+        }
+        if (value <= nxt->value()) {
+          break;
+        }
+        widths[i] += ptr->get_count(i);
+        ptr = nxt;
+      }
+      update[i] = ptr;
+    }
+    if (nxt != nullptr && nxt->value() == value) {
+      new_level = -1;
+    }
+    Node* inserted = new Node(value, new_level);
+    int w = 0;
+    for (int i = 0; i <= new_level; i++) {
+      int count = update[i]->get_count(i);
+      inserted->set_forward(i, update[i]->forward(i), std::max(0, count - w));
+      update[i]->set_forward(i, inserted, w+1);
+      w += widths[i];
+    }
+    for (int i = new_level + 1; i <= level_; i++) {
+      update[i]->inc_count(i);
+    }
+    size_++;
+  }
+
+  int index_of(int value) {
+    Node* pp = head_;
+    int k = 0;
+    int index = 0;
+    for (int i = level_; i >= 0; i--) {
+      Node* ptr = pp;
+      while (ptr != nullptr && (ptr->value() <= value || ptr == head_)) {
+        // std::cout << "[contains check] level " << i << " [" << k << "] " << ptr->value() << " count " << ptr->get_count(i) << std::endl;
+        if (ptr->value() == value) {
+          return index;
+        }
+        pp = ptr;
+        index += ptr->get_count(i);
+        ptr = ptr->forward(i);
+        k++;
+      }
+      index -= pp->get_count(i);
+    }
+    bool result = pp != nullptr && pp->value() == value;
+    if (result) {
+      return index;
+    }
+    return index;
+  }
+
+  void print_nodes() {
+    for (int i = level_; i >= 0; i--) {
+      std::cout << "level " << i << std::endl;
+      Node* ptr = head_;
+      while (ptr != nullptr) {
+        ptr->print(i);
+        ptr = ptr->next(i);
+      }
+      std::cout << std::endl;
+    }
+  }
+
+ private:
+  int level_;
+  int size_;
+  Node* head_;
+};
 } // namespace
 
 int main(int argc, const char** argv) {
@@ -63,25 +266,23 @@ int main(int argc, const char** argv) {
 
   bool debug = false;
 
-  std::set<int> binary;
+  SkipList binary;
   int count = 0;
   for (const auto& e : events) {
     if (e.type == 0) {
       // std::cout << "1" << std::endl;
       binary.insert(e.y1);
     } else if (e.type == 1) {
-      // std::cout << "2" << std::endl;
-      int k = 0;
-      auto it = binary.lower_bound(e.y1);
-      while(it != binary.end() && *it <= e.y2) {
-        k++;
-        it++;
-      }
+      int idx1 = binary.index_of(e.y1);
+      int idx2 = binary.index_of(e.y2);
+      int k = (idx2 - idx1);
+      binary.print_nodes();
+      std::cout << "++ " << e.y1 << " " << e.y2 << " == " << k << std::endl;
       // int k = binary.distance(y1, y2);
       count += k;
     } else {
       // std::cout << "erase " << e.y1 << std::endl;
-      binary.erase(e.y1);
+      binary.remove(e.y1);
     }
   }
 
